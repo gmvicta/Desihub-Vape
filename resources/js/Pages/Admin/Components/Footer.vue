22c0c51f630da2aada0e68c2aa483e750b95761c
Glen Victa <glenvicta@gmail.com>
2025-02-27 22:20:05 +0800

    first commit

---
+++ resources/js/Pages/Admin/Components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="bg-gray-800 text-white text-center p-4">
+      <p>© 2025 Desipulos Vape Hub. All rights reserved.</p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: "FooterComponent"
+  };
+  </script>
+  
+  <style scoped>
+  footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+  </style>
+  
---
+++ resources/js/Pages/User/Layouts/Hero.vue
@@ -0,0 +1,11 @@
+<template>
+     <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+            <h1
+                class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                DESIHUB</h1>
+            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at
+                Desiplos Vape Hub is a modern vape shop management system that streamlines inventory tracking, sales analytics, RFID-based transactions, loyalty rewards, and sentiment analysis. Built with Laravel 10 and Vue 3, it enhances customer experience while optimizing business operations.</p>
+        </div>
+    </section>
+</template>
